9ccd2262a13917f6a2e93d26f61f6db6a485c12e
Rishabh <rishabhtiwari3538@gmail.com>
2026-02-24 15:26:18 +0530

    feat: Add initial application settings file.

--- ecommerce_app/settings.py
+++ ecommerce_app/settings.py
@@ -134,13 +134,18 @@ USE_I18N = True
 # ──────────────────────────────────────────────
 # EMAIL (from .env)
 # ──────────────────────────────────────────────
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'sunnyk28912@gmail.com')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'Admin@123')
-DEFAULT_FROM_EMAIL = f'R.onny <{EMAIL_HOST_USER}>'
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'admin@store.local'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+    EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+    EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'sunnyk28912@gmail.com')
+    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'Admin@123')
+    DEFAULT_FROM_EMAIL = f'R.onny <{EMAIL_HOST_USER}>'
+
 EMAIL_SUBJECT_PREFIX = '[R.onny] '
 
 # ──────────────────────────────────────────────
